c009158a339476997fc42c4b0f0a9857389e1131
Racheal5777 <rachealtifr@gmail.com>
2025-11-22 01:42:19 -0800

    serializer and email settings with media

---
+++ userapi/migrations/utils.py
@@ -0,0 +1,19 @@
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def send_email(username, email):
+    subject = "Welcome New Yale User"
+    body = f'''Hello {username}! Your registration has been completed.
+Please reach out to our dev team for further information.
+For any complaints, kindly reach out to our customer rep.
+'''
+
+    send_mail(
+        subject,
+        body,
+        settings.EMAIL_HOST_USER,
+        [email],
+        fail_silently=False,
+    )
+        
